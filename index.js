1d5945f056937ef617d2729bd640f69ca79ed219
Hacene010 <hamrounehacene06@gmail.com>
2021-06-02 17:07:30 +0200

    created connection between front-back-db and some routes

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express');
+const cors = require('cors');
+const connection = require('./db-config');
 
 const app = express();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use(cors());
+app.get('/texts', async (req, res) => {
+  connection.query('SELECT * FROM texts', (err, rows) => {
+    if (err) {
+      res.status(500).send('Error retrieving data from database !');
+    } else {
+      const results = {};
+      for (let i = 0; i < rows.length; i += 1) {
+        const row = rows[i];
+        const myKey = row.tagname;
+        const myValue = row.fr;
+        results[myKey] = myValue;
+      }
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get('/images', async (req, res) => {
+  connection.query('SELECT * FROM images', (err, results) => {
+    if (err) {
+      res.status(500).send('Error retrieving data from database !');
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 app.use('/', (req, res) => {
   res.status(404).send('Route not found! ');
 });
 
-app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+app.listen(port, () => {
+  console.log(`API now available on http://localhost:${port}!`);
 });
