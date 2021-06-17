22c5c1c3177d761f10dfe97a584d88000cecbfb7
yulen59 <j.delemarle@gmail.com>
2021-06-17 10:15:35 +0200

    add date to texts table

--- data/db.sql
+++ data/db.sql
@@ -69,32 +69,6 @@ INSERT INTO `external_links` VALUES (1,'link_helloasso','https://www.helloasso.c
 UNLOCK TABLES;
 
 --
--- Table structure for table `externelinks`
---
-
-DROP TABLE IF EXISTS `externelinks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `externelinks` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `tagname` varchar(64) NOT NULL,
-  `link_to` varchar(512) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `externelinks`
---
-
-LOCK TABLES `externelinks` WRITE;
-/*!40000 ALTER TABLE `externelinks` DISABLE KEYS */;
-INSERT INTO `externelinks` VALUES (1,'link_helloasso','https://www.helloasso.com/associations/edtech-hauts-de-france'),(2,'link_newsletter','http://eepurl.com/hhJSqj'),(3,'link_facebook','https://www.facebook.com/Edtech.HdF'),(4,'link_linkedIn','https://www.linkedin.com/company/edtech-hauts-de-france/'),(5,'link_twitter','https://twitter.com/edtech_hdf');
-/*!40000 ALTER TABLE `externelinks` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `images`
 --
 
@@ -162,7 +136,7 @@ CREATE TABLE `partners` (
   `logo_alt` varchar(64) DEFAULT NULL,
   `link` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,6 +145,7 @@ CREATE TABLE `partners` (
 
 LOCK TABLES `partners` WRITE;
 /*!40000 ALTER TABLE `partners` DISABLE KEYS */;
+INSERT INTO `partners` VALUES (1,'Bande Ka','/img/partners/BandeKa.png','BandeKa','https://pod.univ-lille.fr/bande-ka/'),(2,'C2RP','/img/partners/C2RP.png','C2RP','https://www.c2rp.fr/'),(3,'EdtechFrance','/img/partners/EdtechFrance.jpg','EdtechFrance','https://edtechfrance.fr/'),(4,'EFAP','/img/partners/EFAP.png','EFAP','https://www.efap.com/'),(5,'Euratechnologies','/img/partners/Euratechnologies.jpg','Euratechnologies','https://www.euratechnologies.com/'),(6,'FrenchtechLille','/img/partners/FrenchtechLille.png','FrenchtechLille','https://ftlille.com/'),(7,'HangarK','/img/partners/HangarK.png','HangarK','https://www.hangark.be/'),(8,'Nordledépartement','/img/partners/NordleDépartement.jpg','Nord','https://lenord.fr/jcms/j_6/accueil'),(9,'PlaineImages','/img/partners/PlaineImages.gif','PlaineImages','https://www.plaine-images.fr/'),(10,'RégionHautsdeFrance','/img/partners/RégionHdF.png','RegionHautsdeFrance','https://www.hautsdefrance.fr/'),(11,'UniversitédeLille','/img/partners/UniversitédeLille.png','UniversitédeLille','https://www.univ-lille.fr/');
 /*!40000 ALTER TABLE `partners` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +162,7 @@ CREATE TABLE `settings_carousel` (
   `value` varchar(64) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -196,7 +171,7 @@ CREATE TABLE `settings_carousel` (
 
 LOCK TABLES `settings_carousel` WRITE;
 /*!40000 ALTER TABLE `settings_carousel` DISABLE KEYS */;
-INSERT INTO `settings_carousel` VALUES (1,'slider_duration','1000'),(2,'slider_transitionDuration','500'),(3,'slider_infinite','true');
+INSERT INTO `settings_carousel` VALUES (1,'slider_duration','3500'),(2,'slider_transitionDuration','500'),(3,'slider_infinite','true'),(4,'slider_indicators','true'),(5,'carousel_duration','4000'),(6,'items_number','4');
 /*!40000 ALTER TABLE `settings_carousel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -249,7 +224,7 @@ CREATE TABLE `texts` (
 
 LOCK TABLES `texts` WRITE;
 /*!40000 ALTER TABLE `texts` DISABLE KEYS */;
-INSERT INTO `texts` VALUES (1,'first_btn','Actualités'),(2,'second_btn','J\'adhère'),(3,'third_btn','Contact'),(4,'first_section','Cartographie des membres'),(5,'second_section','Membres'),(6,'third_section','Partenaires'),(7,'fourth_section','Actualités'),(8,'fifth_section','Contact'),(9,'cgu','CGU / Mentions légales'),(10,'date','©️2021');
+INSERT INTO `texts` VALUES (1,'first_btn','Actualités'),(2,'second_btn','J\'adhère'),(3,'third_btn','Contact'),(4,'first_section','Cartographie des membres'),(5,'second_section','Membres'),(6,'third_section','Partenaires'),(7,'fourth_section','Actualités'),(8,'fifth_section','Contact'),(9,'cgu','CGU / Mentions légales'),(10,'date','©️ 2021');
 /*!40000 ALTER TABLE `texts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -288,4 +263,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-16 16:23:44
+-- Dump completed on 2021-06-17  9:57:59
