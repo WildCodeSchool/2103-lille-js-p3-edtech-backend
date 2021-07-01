cd497f978ea57d186dad0e584eecaa3a8f18d3c1
Hacene010 <hamrounehacene06@gmail.com>
2021-07-01 21:45:04 +0200

    created a route to actus table

---
+++ routes/actus.js
@@ -0,0 +1,18 @@
+const actusRouter = require('express').Router();
+const connection = require('../db-config');
+
+actusRouter.get('/', (req, res) => {
+  connection.query(
+    'SELECT title, img_src, img_alt, link, content, date_time, is_active FROM actus ORDER BY date_time DESC',
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error retrieving data from database !');
+      } else {
+        res.status(200).json(rows);
+      }
+    }
+  );
+});
+
+module.exports = actusRouter;
