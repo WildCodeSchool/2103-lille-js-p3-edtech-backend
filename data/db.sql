f17445b3bda5db5279820c085c4dd71738677c2f
Georges FORTRY <gfortry@hotmail.com>
2021-07-23 14:18:05 +0200

    add images and texts

--- data/db.sql
+++ data/db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: edtech
 -- ------------------------------------------------------
--- Server version	8.0.25-0ubuntu0.20.04.1
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,7 +32,7 @@ CREATE TABLE `actus` (
   `dateTime` datetime DEFAULT NULL,
   `isActive` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `colors` (
 
 LOCK TABLES `colors` WRITE;
 /*!40000 ALTER TABLE `colors` DISABLE KEYS */;
-INSERT INTO `colors` VALUES (1,'navbar_text_color','#2c2875'),(2,'navbar_background_color','#FFFFFF'),(3,'header_text_color',NULL),(4,'header_background_color',NULL),(5,'slider_text_color','#FFFFFF'),(6,'slider_background_color','rgba(55, 106, 182, 0.664)'),(7,'map_background_color','#2c2875'),(8,'map_text_color','#FFFFFF'),(9,'map_button_background_color','#e61937'),(10,'map_button_text_color','#FFFFFF'),(11,'members_text_color','#2c2875'),(12,'members_background_color','#FFFFFF'),(13,'partners_text_color','#2c2875'),(14,'partners_background_color','#FFFFFF'),(15,'hdfTwitter_text_color','#FFFFFF'),(16,'hdfTwitter_background_color','#e61937'),(17,'contact_text_color','#FFFFFF'),(18,'contact_background_color','#2c2875'),(19,'footer_text_color','#FFFFFF'),(20,'footer_background_color','#1b175b'),(21,'contact_button_background_color','#e61937'),(22,'contact_button_text_color','#FFFFFF'),(23,'contact_button_background_color_inactive','#2aa11d'),(24,'contact_button_text_color_inactive','#FFFFFF'),(25,'contact_button_background_color_hover','#FF1919'),(26,'newsletter_background_color','#FFFFFF'),(27,'newsletter_text_color','#2c2875'),(28,'newsletter_button_background_color','#e61937'),(29,'newsletter_button_text_color','#FFFFFF'),(30,'actu_background_color','#2c2875'),(31,'actu_link_color','#FFFFFF'),(32,'accordion_background_color','#2c2875'),(33,'accordion_text_color','#FFFFFF'),(34,'contact_background_alert','#e61937'),(35,'contact_background_alert_bar','#FFFFFF'),(36,'contact_text_alert','#FFFFFF');
+INSERT INTO `colors` VALUES (1,'navbar_text_color','#2c2875'),(2,'navbar_background_color','#FFFFFF'),(3,'header_text_color',NULL),(4,'header_background_color',NULL),(5,'slider_text_color','#FFFFFF'),(6,'slider_background_color','rgba(55, 106, 182, 0.5)'),(7,'map_background_color','#2c2875'),(8,'map_text_color','#FFFFFF'),(9,'map_button_background_color','#e61937'),(10,'map_button_text_color','#FFFFFF'),(11,'members_text_color','#2c2875'),(12,'members_background_color','#FFFFFF'),(13,'partners_text_color','#2c2875'),(14,'partners_background_color','#FFFFFF'),(15,'hdfTwitter_text_color','#FFFFFF'),(16,'hdfTwitter_background_color','#e61937'),(17,'contact_text_color','#FFFFFF'),(18,'contact_background_color','#2c2875'),(19,'footer_text_color','#FFFFFF'),(20,'footer_background_color','#1b175b'),(21,'contact_button_background_color','#e61937'),(22,'contact_button_text_color','#FFFFFF'),(23,'contact_button_background_color_inactive','#2aa11d'),(24,'contact_button_text_color_inactive','#FFFFFF'),(25,'contact_button_background_color_hover','#FF1919'),(26,'newsletter_background_color','#FFFFFF'),(27,'newsletter_text_color','#2c2875'),(28,'newsletter_button_background_color','#e61937'),(29,'newsletter_button_text_color','#FFFFFF'),(30,'actu_background_color','#2c2875'),(31,'actu_link_color','#FFFFFF'),(32,'accordion_background_color','#2c2875'),(33,'accordion_text_color','#FFFFFF'),(34,'contact_background_alert','#e61937'),(35,'contact_background_alert_bar','#FFFFFF'),(36,'contact_text_alert','#FFFFFF');
 /*!40000 ALTER TABLE `colors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +84,7 @@ CREATE TABLE `external_links` (
   `linkTo` varchar(512) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,7 +111,7 @@ CREATE TABLE `images` (
   `alt` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +120,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,'logo_header','/img/header/logo.png','logo'),(2,'picto_newsletter','/img/navBar/pictogrammes/Picto_Newsletter.png','picto_newsletter'),(3,'picto_facebook','/img/navBar/pictogrammes/Picto_Facebook.png','picto_facebook'),(4,'picto_linkedIn','/img/navBar/pictogrammes/Picto_Linkedin.png','picto_linkedIn'),(5,'picto_twitter','/img/navBar/pictogrammes/Picto_Twitter.png','picto_twitter'),(6,'logo_footer','/img/footer/Logo_EdTech_HdF_Typo.jpeg','picto_footer'),(7,'img_map','https://images-ext-2.discordapp.net/external/axV4qu9QBHztGjzsXzkSSh7lXPuoN3Cvv4iQmidOUAE/%3Fw%3D1120%26h%3D490%26q%3D70%26fm%3Dwebp%26fit%3Dfill/https/images.france.fr/zeaejvyq9bhj/dAZLYQVKQoU6a8gUeU0Mi/02f9938c1c421a313d6b238f670d2ab6/article_ImP_Lille-NordFrance_1120x490.jpg','image_map'),(8,'header_background_img','https://images.france.fr/zeaejvyq9bhj/dAZLYQVKQoU6a8gUeU0Mi/02f9938c1c421a313d6b238f670d2ab6/article_ImP_Lille-NordFrance_1120x490.jpg?w=1120&h=490&q=70&fm=webp&fit=fill','background_img');
+INSERT INTO `images` VALUES (1,'logo_header','/img/header/logo.png','logo'),(2,'picto_newsletter','/img/navBar/pictogrammes/Picto_Newsletter.png','picto_newsletter'),(3,'picto_facebook','/img/navBar/pictogrammes/Picto_Facebook.png','picto_facebook'),(4,'picto_linkedIn','/img/navBar/pictogrammes/Picto_Linkedin.png','picto_linkedIn'),(5,'picto_twitter','/img/navBar/pictogrammes/Picto_Twitter.png','picto_twitter'),(6,'logo_footer','/img/footer/Logo_EdTech_HdF_Typo.jpeg','picto_footer'),(7,'img_map','/img/HDFcarte.jpg','image_map'),(8,'header_background_img','https://images.france.fr/zeaejvyq9bhj/dAZLYQVKQoU6a8gUeU0Mi/02f9938c1c421a313d6b238f670d2ab6/article_ImP_Lille-NordFrance_1120x490.jpg?w=1120&h=490&q=70&fm=webp&fit=fill','background_img');
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,7 +138,7 @@ CREATE TABLE `members` (
   `logoAlt` varchar(64) DEFAULT NULL,
   `link` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +147,7 @@ CREATE TABLE `members` (
 
 LOCK TABLES `members` WRITE;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
-INSERT INTO `members` VALUES (1,'tabuleo','/img/members/Tabuleo.png','tabuleo','https://www.tabuleo.com/'),(2,'orientoi','/img/members/Orientoi.jpg','orientoi','https://www.orientoi.fr/'),(3,'wild_code_school','/img/members/WildCodeSchool.png','wildcodeschool','https://www.wildcodeschool.com/fr-FR'),(4,'agence_dad','/img/members/Agencedad.jpg','agencedad','https://www.dad-agence.fr/'),(5,'cantoo_scribe','/img/members/CantooScribe.png','cantooscribe','https://www.cantoo.fr/'),(6,'edhec','/img/members/EDHEC.jpg','edhec','https://www.edhec.edu/fr');
+INSERT INTO `members` VALUES (1,'tabuleo','/img/members/Tabuleo.png','tabuleo','https://www.tabuleo.com/'),(2,'orientoi','/img/members/Orientoi.jpg','orientoi','https://www.orientoi.fr/'),(3,'wild_code_school','/img/members/WildCodeSchool.png','wildcodeschool','https://www.wildcodeschool.com/fr-FR'),(4,'agence_dad','/img/members/Agencedad.jpg','agencedad','https://www.dad-agence.fr/'),(5,'cantoo_scribe','/img/members/CantooScribe.png','cantooscribe','https://www.cantoo.fr/'),(6,'edhec','/img/members/EDHEC.jpg','edhec','https://www.edhec.edu/fr'),(8,'babaoo','/img/members/Babaoo.png','Babaoo',NULL),(9,'be_smart','/img/members/BeSMART-edu.png','BeSmart',NULL),(10,'corneille','/img/members/Corneille.png','Corneille',NULL),(11,'edukeasy','/img/members/edukeasy.png','edukeasy',NULL),(12,'gowizyou','/img/members/GoWizYou.png','GoWizYou',NULL),(13,'ID6','/img/members/ID6.png','ID6',NULL),(14,'junia','/img/members/Junia.png','Junia',NULL),(15,'KOoOl','/img/members/KOoOl.png','KOoOl',NULL),(16,'Kwartz','/img/members/Kwartz.png','Kwartz',NULL),(17,'Neuronys','/img/members/Neuronys.png','Neuronys',NULL),(18,'Niryo','/img/members/Niryo.png','Niryo',NULL),(19,'Popschool','/img/members/Popschool.png','Popschool',NULL),(20,'Skema','/img/members/Skema.png','Skema',NULL),(21,'Skill_you','/img/members/Skill_You.png','Skill_You',NULL),(22,'Trézorium','/img/members/Trézorium.png','Trézorium',NULL),(23,'Whispeak','/img/members/Whispeak.png','Whispeak',NULL),(24,'Wooclap','/img/members/Wooclap.png','Wooclap',NULL);
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +165,7 @@ CREATE TABLE `partners` (
   `logoAlt` varchar(64) DEFAULT NULL,
   `link` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -217,7 +217,7 @@ CREATE TABLE `settings_carousel` (
   `value` varchar(64) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,12 +239,12 @@ DROP TABLE IF EXISTS `slider`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `slider` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) NOT NULL,
+  `title` varchar(128) NOT NULL,
   `imgSrc` varchar(128) NOT NULL,
   `imgAlt` varchar(64) DEFAULT NULL,
   `subtext` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -253,7 +253,7 @@ CREATE TABLE `slider` (
 
 LOCK TABLES `slider` WRITE;
 /*!40000 ALTER TABLE `slider` DISABLE KEYS */;
-INSERT INTO `slider` VALUES (1,'Lorem ipsum 1','/img/slider/bureau1.jpeg','slide','1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quaerat necessitatibus voluptatem eos amet qui sit at a magnam nihil ullam numquam labore, accusantium ducimus est quo, debitis dolorum cumque.'),(2,'Lorem ipsum 2','/img/slider/bureau2.jpeg','slide','2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quaerat necessitatibus voluptatem eos amet qui sit at a magnam nihil ullam numquam labore, accusantium ducimus est quo, debitis dolorum cumque.'),(3,'Lorem ipsum 3','/img/slider/bureau3.jpeg','slide','3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quaerat necessitatibus voluptatem eos amet qui sit at a magnam nihil ullam numquam labore, accusantium ducimus est quo, debitis dolorum cumque.');
+INSERT INTO `slider` VALUES (1,'Mettre la technologie au service de l’éducation et de la formation.','/img/slider/school.jpg','school','L’association EdTech France fédère les entrepreneurs français qui ont décidé de rendre la technologie et l’innovation utiles à l’éducation, à l’enseignement supérieur et à la formation tout au long de la vie.'),(2,'Repenser les apprentissages à l’ère du numérique.','/img/slider/students.jpg','slide','L’association EdTech France rassemble tous ceux qui pensent que la technologie peut aider à répondre aux nouveaux défis du paysage numérique, au service des apprenants et des formateurs.'),(3,'Développer les compétences tout au long de la vie.','/img/slider/presentation.jpg','slide','L’association EdTEch France rapprochent entreprises innovantes, organismes de formation et acteurs institutionnels pour promouvoir l’usage du numérique au service du développement des compétences.');
 /*!40000 ALTER TABLE `slider` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -270,7 +270,7 @@ CREATE TABLE `texts` (
   `fr` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagname_UNIQUE` (`tagname`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -279,7 +279,7 @@ CREATE TABLE `texts` (
 
 LOCK TABLES `texts` WRITE;
 /*!40000 ALTER TABLE `texts` DISABLE KEYS */;
-INSERT INTO `texts` VALUES (1,'first_btn','Actualités'),(2,'second_btn','J\'adhère'),(3,'third_btn','Contact'),(4,'first_section','Cartographie des membres'),(5,'second_section','Membres'),(6,'third_section','Partenaires'),(7,'fourth_section','Actualités'),(8,'fifth_section','Contact'),(9,'description_map','Lorem ipsum dolor sit amet consectetur adipis'),(10,'cgu','CGU / Mentions légales'),(11,'date','©️2021'),(12,'first_input','Prénom *:'),(13,'second_input','Nom*:'),(14,'third_input','Structure :'),(15,'fourth_input','Numéro de téléphone :'),(16,'fifth_input','Adresse mail *:'),(17,'sixth_input','Message *:'),(19,'requirable_first_input','obligatoire'),(20,'requirable_second_input','obligatoire'),(21,'requirable_third_input','non obligatoire'),(28,'requirable_fourth_input','non obligatoire'),(29,'requirable_fifth_input','obligatoire'),(30,'requirable_sixth_input','obligatoire'),(31,'contact_catchPhrase','Lorem, ipsum dolor sit amet consectetur...'),(32,'edtechhdf_title','Edtech Hauts-de-France'),(33,'edtech_adress','165 Avenue de Bretagne'),(34,'edtech_city','59000 Lille'),(35,'adress_field1',''),(36,'adress_field2',''),(37,'adress_field3',''),(38,'adress_field4',''),(39,'isImgMapFirst','false'),(40,'btn_map','Lien vers la carte'),(54,'RGPD','J\'accepte de partager mes informations à l\'association edTech HdF.'),(55,'contact_messageSent','Message envoyé !'),(56,'contact_sendMessage','Envoyer'),(57,'sixth_section','Newsletter'),(58,'description_newsletter','Abonnez-vous à notre newsletter !'),(59,'btn_newsletter','Je m\'inscris !'),(60,'fourth_btn','Présentation');
+INSERT INTO `texts` VALUES (1,'first_btn','Actualités'),(2,'second_btn','J\'adhère'),(3,'third_btn','Contact'),(4,'first_section','Cartographie des membres'),(5,'second_section','Membres'),(6,'third_section','Partenaires'),(7,'fourth_section','Actualités'),(8,'fifth_section','Contact'),(9,'description_map','Consultez la cartographie de nos membres'),(10,'cgu','CGU / Mentions légales'),(11,'date','©️2021'),(12,'first_input','Prénom *:'),(13,'second_input','Nom*:'),(14,'third_input','Structure :'),(15,'fourth_input','Numéro de téléphone :'),(16,'fifth_input','Adresse mail *:'),(17,'sixth_input','Message *:'),(19,'requirable_first_input','obligatoire'),(20,'requirable_second_input','obligatoire'),(21,'requirable_third_input','non obligatoire'),(28,'requirable_fourth_input','non obligatoire'),(29,'requirable_fifth_input','obligatoire'),(30,'requirable_sixth_input','obligatoire'),(31,'contact_catchPhrase','N\'hésitez pas à nous contacter si vous souhaitez plus d\'informations... '),(32,'edtechhdf_title','Edtech Hauts-de-France'),(33,'edtech_adress','165 Avenue de Bretagne'),(34,'edtech_city','59000 Lille'),(35,'adress_field1',''),(36,'adress_field2',''),(37,'adress_field3',''),(38,'adress_field4',''),(39,'isImgMapFirst','false'),(40,'btn_map','Lien vers la carte'),(54,'RGPD','J\'accepte de partager mes informations à l\'association edTech HdF.'),(55,'contact_messageSent','Message envoyé !'),(56,'contact_sendMessage','Envoyer'),(57,'sixth_section','Newsletter'),(58,'description_newsletter','Abonnez-vous à notre newsletter !'),(59,'btn_newsletter','Je m\'inscris !'),(60,'fourth_btn','Présentation');
 /*!40000 ALTER TABLE `texts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -320,4 +320,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-20 14:06:08
+-- Dump completed on 2021-07-23 14:16:46
